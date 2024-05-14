9377645d6f389ca385d3ea4afa3ec0e0a2877e9a
Eric <eric.schubert.3d@gmail.com>
2024-05-14 20:59:56 +0200

    Added collide and slide algorithm with basic functionality

--- Skategame/Scripts/characterController.gd
+++ Skategame/Scripts/characterController.gd
@@ -1,60 +1,99 @@
 extends Node3D
 
-const acc = 0.5
-const grav = 0.5
-const jmpAcc = 0.2
-const rot = 10
+const maxVel = 5.0
+const acc = 35.0
+const rot = 1.0
+const jump = 7.0
+const maxBounces = 8
+const skinWidth = 0.01
+const maxSlopeAngle = 55
 
-@export var floorC:RayCast3D = null
+const gravity = Vector3.DOWN * 0.1
 
 var input = Vector3.ZERO
-var dir = Vector3.ZERO
+var jumping = false
+
+var shapeCast: ShapeCast3D
+var fwdRay: RayCast3D
+@export var debugView: Control
+
+var tempVector = Vector3.ZERO
 var vel = Vector3.ZERO
-var isOnFloor = false
 var xForm = null
+var spd = 0.0
+
+func _ready():
+	shapeCast = get_node("ShapeCast3D")
+	fwdRay = get_node("RayCast3DFwd")
+	xForm = global_transform.basis
 
 func _physics_process(delta):
+	xForm = global_transform.basis
+	vel = xForm.z	* spd
+	spd += input.y * 0.01
 	_inputHandler()
+	rotate_y(input.x * 5.0 * delta)
+	tempVector = _move(xForm.z * spd * delta)	
+	global_position += _move(xForm.z * spd * delta)	
 	
-	dir = global_transform.basis
-	if(floorC.is_colliding()):
-		isOnFloor = true
-	else:
-		isOnFloor =false
+func _collideAndSlide(vel, pos, depth, gravityPass, velInit):
+	if(depth >= maxBounces):
+		return Vector3.ZERO	
+	#var dist = vel.length() + skinWidth
+	#raycasting logic
+	var space_state = get_world_3d().direct_space_state
+	var origin = pos
+	var end = (vel.normalized() * (vel.length())) + pos
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	var result = space_state.intersect_ray(query)
+	#if ray hit anything calculating new position
+	if(result):	
+		var snapToSurface = vel.normalized() * (pos.distance_to(result.position) - skinWidth)
+		var leftover = vel - snapToSurface
 		
-	if(isOnFloor):
-		xForm = global_transform.basis
-		global_transform.basis = lerp(xForm,_groundAlign(xForm, floorC.get_collision_normal()),  delta * 30)
-		#killing vertical velocity when grounded
-		vel += Vector3.UP * vel.dot(Vector3.DOWN)
-		#rotation input
-		if input .x != 0:
-			rotate_y(input.x * rot * delta * 0.1)
-		#acceleration input and friction
-		if(input.y != 0):
-			vel += xForm.z * input.y * delta * acc
+		var angle = Vector3.UP.angle_to(result.normal)
+		
+		if (snapToSurface.length() <= skinWidth):
+			snapToSurface = Vector3.ZERO
+		#on normal ground
+		if (angle <= maxSlopeAngle):
+			if(gravityPass):
+				return snapToSurface #todo, fix gravity!
+			leftover = _projectAndScale(leftover, result.normal)
+		#wall or steep slope
 		else:
-			vel *= 0.95
-		
-		#jumping handling
-		if(input.z):
-			vel += Vector3.UP * jmpAcc
-				#killing vertical velocity, when grounded
-		
-		vel = _killOrthogonalVelocity(xForm, vel)
+			var scale = 1 - Vector3(result.normal.x, 0, result.normal.z).normalized().dot(-Vector3(velInit.x, 0, velInit.z).normalized())
+			leftover = _projectAndScale(leftover, result.normal) * scale
+		#return snapToSurface + leftover	
+		return 	 snapToSurface + _collideAndSlide(leftover,pos + snapToSurface , depth + 1, gravityPass, velInit)
+	return vel
 	
-	else:
-		#rotation input while flying
-		if input .x != 0:
-			rotate_y(input.x * rot * delta)
-		vel += Vector3.DOWN * grav * delta
-		
-	position += vel
+func _move(velocity):
+	velocity = _collideAndSlide(velocity, global_position, 0, false, velocity)
+	velocity += _collideAndSlide(gravity, global_position + velocity, 0, true, gravity)
+	return velocity
+
+func _projectAndScale(leftover, normal):
+	var mag = leftover.length()
+	leftover = Plane(normal, Vector3.UP).project(leftover).normalized()
+	leftover *= mag	
+	return leftover	
 
 func _inputHandler():
 	input.x = int(Input.is_action_pressed("Left")) - int(Input.is_action_pressed("Right"))
 	input.y = int(Input.is_action_pressed("Forward")) - int(Input.is_action_pressed("Backward"))
 	input.z = int(Input.is_action_just_pressed("Jump"))
+	
+func _killOrthogonalVelocity(xform, vel):
+	var fwd_vel =xform.z * vel.dot(xform.basis.z)
+	var ort_vel = xform.x * vel.dot(xform.basis.x)
+	var up_vel = Vector3.UP * vel.dot(xform.basis.y)
+	vel = fwd_vel + ort_vel * 0.75 + up_vel
+	return vel
+	
+func _isGrounded():
+	pass
 
 func _groundAlign(xform, normal):
 	xform.y = normal
@@ -62,9 +101,3 @@ func _groundAlign(xform, normal):
 	xform = xform.orthonormalized()
 	return xform
 
-func _killOrthogonalVelocity(xForm, vel):
-	var fwdVel = xForm.z * vel.dot(xForm.z)
-	var ortVel = xForm.x * vel.dot(xForm.x)
-	var upVel = Vector3.UP * vel.dot(Vector3.UP)
-	vel = fwdVel + ortVel * 0.95 + upVel
-	return vel
--- Skategame/Scripts/debugView.gd
+++ Skategame/Scripts/debugView.gd
@@ -7,16 +7,17 @@ func _process(delta):
 	queue_redraw()
 
 func _draw():	
-	_debugDraw(player.global_position, player.global_position + player.vel * 10, Color.PURPLE)
-	_debugDraw(player.global_position, player.global_position + Vector3.UP, Color.RED)
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.z, Color.GREEN)
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED)
-	_debugDraw(player.global_position, player.global_position - player.transform.basis.y, Color.ORANGE)
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.x, Color.BLUE)
+	#player.global_position+ player.global_transform.basis.z
+	_debugDraw(player.global_position , player.global_position + player.vel , Color.GREEN)
+	_debugDraw(player.global_position , player.global_position + player.tempVector * 50, Color.RED)
+	#_debugDraw(player.global_position, player.global_position + Vector3.UP, Color.RED)
+	#_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED)
+	#_debugDraw(player.global_position, player.global_position - player.transform.basis.y, Color.ORANGE)
+	#_debugDraw(player.global_position, player.global_position + player.transform.basis.x, Color.BLUE)
 	pass
 	
 func _debugDraw(from, to, col):
-	draw_line(camera.unproject_position(from), camera.unproject_position(to), col, 2.0)
+	draw_line(camera.unproject_position(from), camera.unproject_position(to), col, 4.0)
 
 
 
