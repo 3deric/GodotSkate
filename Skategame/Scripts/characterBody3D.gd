a0df680dc0a0a2d31a0ab1db699c58bcd7634ae4
Eric <eric.schubert.3d@gmail.com>
2024-05-19 22:26:24 +0200

    Refactored the player controller, simplified some functions

--- Skategame/Scripts/characterBody3D.gd
+++ Skategame/Scripts/characterBody3D.gd
@@ -2,15 +2,24 @@ extends CharacterBody3D
 
 const acc = 0.2
 const jumpVel = 5.0
-const rot = 1.0
+const rot = 2.0
+const rotJump = 5.0
 const maxVel = 20.0
 const gravity = 10.0
+
+enum PlayerState {RESET, GROUND, AIR, FALL}
 
 var input = Vector3.ZERO #input values
 var dir = Vector3.ZERO #current direction of motion
 var lastDir = Vector3.ZERO #direction of last frame for falling check
 var xForm = null
 var grounded = false
+var right = Vector3.ZERO
+
+var playerState = PlayerState.RESET
+
+var rampPos = Vector3.ZERO
+var rampDir = Vector3.ZERO
 @onready var rbdBoard: RigidBody3D = get_node("RBDBoard")
 @onready var rbdChar: RigidBody3D = get_node("RBDCharacter")
 var fallen = false
@@ -20,92 +29,125 @@ func _ready():
 
 func _physics_process(delta):
 	xForm = global_transform
-	dir = xForm.basis.x.cross(up_direction)
-	grounded = is_on_floor()
-	#get inputs
+	
+
+	#print(abs(velocity.normalized().dot(Vector3.UP)) )
+	#check if player is falling	
 	_inputHandler()	
+	_playerState()
 	
-	#print(get_last_slide_collision())
-	
-	if fallen:
-		if input.y:
-			_resetPlayer(Vector3.UP * 5.0)
+	if (playerState == PlayerState.FALL):
+		velocity = Vector3.ZERO
 		return
 	
-
-	if grounded:	
-		#print(abs(lastDir.dot(dir)))
-		if (abs(lastDir.dot(dir)) < 0.5 and abs(lastDir.dot(Vector3.UP)) < 0.95 and !fallen):
-			_fall()
-		#print(get_floor_normal())
-		var raycast = _raycast(global_position, global_position - up_direction)
-		if raycast:
-			up_direction = raycast.normal
-		if input.x:
-			#rotation input
-			#velocity = velocity.rotated(up_direction, input.x * rot)
-			rotate_y(input.x * rot * delta)	
-		if input.y:
-			velocity += dir * input.y * acc
-			
-		else:
-			velocity *= 0.99
-		if input.z:
-			velocity += up_direction * jumpVel
-			lastDir = velocity.normalized()
-		else:
-			lastDir = dir
-		velocity = _killOrthogonalVelocity(xForm, velocity)
+	#behaviour while grounded
+	if (playerState == PlayerState.GROUND):
 		
-	else:
-		if input.x:
-			rotate_y(input.x * rot * delta * 10)
-		up_direction = Vector3.UP
+		_setUpDirection()
+		
+		#acceleration
+		velocity += xForm.basis.z * input.y * acc
+		
+		#jump acceleration
+		velocity += xForm.basis.y * input.z * jumpVel
+	
+		rotate_object_local(Vector3.UP, input.x * rot * delta)
 
-	#apply gravity
-	velocity.y -= gravity * delta
+		#apply gravity
+		velocity.y -= gravity * delta
+		
+		_killOrthogonalVelocity(xForm, velocity)
+	
+	#movement while not grounded
+	else:
+		rotate_object_local(Vector3.UP, input.x * rotJump * delta)
+		velocity.y -= gravity * delta
+		up_direction = Vector3.UP
+		
+	#align upvector with ground while grounded
+	global_transform = _align(global_transform, up_direction)
+	
 	
 	#apply movement
+
 	move_and_slide()
+
+
+func _playerState():
+	if (playerState == PlayerState.FALL):
+		return
+	
+	if is_on_floor():
+		var fallCheck = (abs(velocity.normalized().dot(xForm.basis.z)))
+		if(fallCheck < 0.5 and fallCheck != 0 and velocity.length() > 1.0):
+			playerState = PlayerState.FALL
+			_fall()
+		else:
+			playerState = PlayerState.GROUND
+	else:
+		playerState = PlayerState.AIR
+		
+func _setUpDirection():
+	#raycast to define new up direction based on the ground
+	var raycast = _raycast(global_position, global_position - up_direction)
+	if raycast:
+		up_direction = raycast.normal
+	else:
+		up_direction = Vector3.UP
 
 func _process(delta):
 	#to do
 	#interpolate rotation to get smoother motion on slopes
-	var basis = Basis(dir.cross(up_direction), up_direction, dir)
-	var transform = Transform3D(basis, global_position)
-	if(!fallen):
-		rbdChar.global_transform = transform
-		rbdBoard.global_transform = transform
+	if(playerState != PlayerState.FALL):
+		rbdChar.global_transform = global_transform
+		rbdBoard.global_transform = global_transform
+		
+func _rampSnap():
+	#to do
+	#check if player is going vertial
+	#set new state to ramp
+	#store last grounded position to cast a ray that snaps to the ramp
+	#move the ray on the height of the ramp to slide the player along the ramp
+	var collisionInfo = get_last_slide_collision()
+	if collisionInfo:
+		var collider = collisionInfo.get_collider()
+		if(collider.collision_layer == 2):
+			#collisionInfo.
+			rampPos = collisionInfo.get_position()
+			rampDir = up_direction
+			
+			pass
+		#if (!get_slide_collision(0) and collider.collision_layer == 2):
+		#	print("from ramp!")
+	pass
 		
 func _fall():
-	fallen = true
-	print("FALL!")
 	rbdChar.freeze = false
 	rbdChar.apply_impulse(velocity)
 	rbdBoard.freeze = false
 	rbdBoard.apply_impulse(velocity)
 	
-	
-func _resetPlayer(position):
+func _resetPlayer(pos):
 	up_direction = Vector3.UP
 	velocity = Vector3.ZERO
 	lastDir = global_transform.basis.x.cross(up_direction)
-	global_position = position
+	global_position = pos
 	rbdChar.freeze = true
 	rbdBoard.freeze = true
-	fallen = false
-	grounded = false
-	print("resetting")
+	playerState = PlayerState.RESET
 
 func _inputHandler():
 	input.x = int(Input.is_action_pressed("Left")) - int(Input.is_action_pressed("Right"))
 	input.y = int(Input.is_action_pressed("Forward")) - int(Input.is_action_pressed("Backward"))
 	input.z = int(Input.is_action_pressed("Jump"))
+	
+	if(input.y and playerState == PlayerState.FALL):
+		_resetPlayer(global_position)
 
-func _killOrthogonalVelocity(xForm, velocity):
-	var fwdVel = xForm.basis.z * velocity.dot(xForm.basis.z)
-	var ortVel = xForm.basis.x * velocity.dot(xForm.basis.x)
-	var upVel = Vector3.UP  * velocity.dot(Vector3.UP)
+func _killOrthogonalVelocity(xForm, vel):
+	var fwdVel = xForm.basis.z * vel.dot(xForm.basis.z)
+	var ortVel = xForm.basis.x * vel.dot(xForm.basis.x)
+	var upVel = xForm.basis.y  * vel.dot(xForm.basis.y)
 	velocity = fwdVel + ortVel * 0.25 + upVel
 	return velocity
 
@@ -114,4 +156,10 @@ func _raycast(from, to):
 	var query = PhysicsRayQueryParameters3D.create(from, to)
 	query.collide_with_areas = true
 	var result = space_state.intersect_ray(query)
-	return result
+	return result   
+
+func _align(xform, newUp):
+	xform.basis.y = newUp
+	xform.basis.x = -xform.basis.z.cross(newUp)
+	xform.basis = xform.basis.orthonormalized()
+	return xform
--- Skategame/Scripts/debugView.gd
+++ Skategame/Scripts/debugView.gd
@@ -6,15 +6,20 @@ extends Control
 func _process(delta):
 	queue_redraw()
 
-func _draw():	
-	_debugDraw(player.global_position, player.global_position + player.velocity * 0.25, Color.PURPLE)
-	_debugDraw(player.global_position, player.global_position + Vector3.UP, Color.RED)
-	if player.grounded:
-		_debugDraw(player.global_position, player.global_position - player.up_direction * 2, Color.PINK)
-	_debugDraw(player.global_position, player.global_position + player.dir, Color.GREEN)
-	_debugDraw(player.global_position, player.global_position + player.lastDir * 1.5, Color.LIGHT_GREEN)
+func _draw():
+	_debugDraw(player.global_position, player.global_position + player.transform.basis.x, Color.GREEN)
 	_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED)
-	_debugDraw(player.global_position, player.global_position + player.up_direction, Color.ORANGE)
+	_debugDraw(player.global_position, player.global_position + player.transform.basis.z, Color.BLUE)
+	#_debugDraw(player.global_position, player.global_position + player.velocity * 0.25, Color.PURPLE)
+	#_debugDraw(player.global_position, player.global_position + Vector3.UP, Color.RED)
+	#if player.grounded:
+	#	_debugDraw(player.global_position, player.global_position - player.up_direction * 2, Color.PINK)
+	#_debugDraw(player.global_position, player.global_position + player.dir, Color.GREEN)
+	#_debugDraw(player.global_position, player.global_position + player.lastDir * 1.5, Color.LIGHT_GREEN)
+	#_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED)
+	#_debugDraw(player.global_position, player.global_position + player.right, Color.BLUE)
+	#_debugDraw(player.global_position, player.global_position + player.up_direction, Color.ORANGE)
+	#_debugDraw(player.rampPos , player.rampPos + player.rampDir, Color.ORANGE)
 	pass
 	
 func _debugDraw(from, to, col):
