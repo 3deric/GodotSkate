c6c7ed99dad1a50f13139806b47bbfeb02bca989
Eric <eric.schubert.3d@gmail.com>
2024-06-22 21:22:15 +0200

    Added grinding behaviour, fixed some bugs

--- Skategame/Scripts/characterBody3D.gd
+++ Skategame/Scripts/characterBody3D.gd
@@ -3,9 +3,9 @@ extends CharacterBody3D
 const acc = 0.2
 const jumpVel = 10.0
 const rot = 2.0
-const rotJump = 5.0
+const rotJump = 7.0
 const maxVel = 25.0
-const gravity = 20.0
+const gravity = 25.0
 const maxBounces = 5
 
 enum PlayerState {RESET, GROUND, PIPE, PIPESNAP, AIR, FALL, GRIND}
@@ -15,8 +15,6 @@ var inputTricks = Vector3.ZERO #input values for tricks
 var dir = Vector3.ZERO #current direction of motion
 var lastDir = Vector3.ZERO #direction of last frame for falling check
 var xForm = null
-var grounded = false
-var right = Vector3.ZERO
 
 var fallTimer = 0.0
 
@@ -34,13 +32,20 @@ var groundNormal = Vector3.RIGHT
 
 var lastCollLayer = 0
 
+var path: Path3D = null
+var pathPosition: float = -1
+var pathLength: float = -1
+var pathDir: int = 0
+var pathVelocity: Vector3 = Vector3.ZERO
+
 @onready var rbdBoard: RigidBody3D = get_node("RBDBoard")
 @onready var rbdChar: RigidBody3D = get_node("RBDCharacter")
-@export var path: Path3D = null
 @onready var area: Area3D = get_node("Area3D")
+@onready var collision: CollisionShape3D = get_node("CollisionShape3D")
 @export var camera: Camera3D = null
 @export var cameraPos: Node3D = null
 @export var ingameUI: Control = null
+
 
 func _ready():
 	_resetPlayer(Vector3.UP * 5.0)
@@ -49,76 +54,83 @@ func _physics_process(delta):
 	xForm = global_transform
 	_inputHandler()	
 	_playerState()
-
 	#debug logic to print the player state only on change
 	if(playerState != lastPlayerState):
 		print(PlayerState.find_key(playerState))
-	lastPlayerState = playerState
-	
+	lastPlayerState = playerState	
 	if (playerState == PlayerState.FALL):
 		velocity *= 0.95
 		move_and_slide()
-		return
-		
+		return		
 	if (playerState == PlayerState.GROUND):
-		lastGroundPos = global_position
-	
+		lastGroundPos = global_position	
 	#behaviour while grounded
-	if (playerState == PlayerState.GROUND or playerState == PlayerState.PIPE):
-		
-		_setUpDirection()
-		
+	if (playerState == PlayerState.GROUND or playerState == PlayerState.PIPE):	
+		_setUpDirection()		
 		if((input.y > 0 and velocity.length() <= maxVel) or (input.y < 0 and velocity.length() >= -maxVel)):
 			velocity += xForm.basis.z * input.y * acc
-		#dedeleration
+		#deceleration
 		else:
-			velocity *= 0.98
-		
-		
+			velocity *= 0.98	
 		#jump acceleration
 		velocity += xForm.basis.y * input.z * jumpVel
-	
 		rotate_object_local(Vector3.UP, input.x * rot * delta)
-
 		#apply gravity
-		velocity.y -= gravity * delta
-		
+		velocity.y -= gravity * delta	
 		_killOrthogonalVelocity(xForm, velocity)
-	
 	#movement while not grounded
 	if (playerState == PlayerState.AIR):
 		rotate_object_local(Vector3.UP, input.x * rotJump * delta)
 		velocity.y -= gravity * delta
-		up_direction = Vector3.UP
-	
+		up_direction = Vector3.UP	
 	#movement while snapped to pipe	
-	#
 	if (playerState == PlayerState.PIPESNAP):
 		rotate_object_local(Vector3.UP, input.x * rotJump * delta)
 		curveSnap = _getClosestCurvePoint(path, global_position)
 		curveTangent = _alignToCurve(path, global_position)
 		up_direction = Vector3.UP.cross(curveTangent)
-		position = Vector3(curveSnap.x, position.y, curveSnap.z)  + up_direction * 0.25
+		position = Vector3(curveSnap.x, position.y, curveSnap.z) + up_direction * 0.25
 		velocity.y -= gravity * delta
+		if (!_getStickCurve(path, global_position)):
+			playerState = PlayerState.AIR		
+	
+	if (playerState == PlayerState.GRIND):
+		collision.disabled = true
+		var grindVel = velocity.length() * 0.99
+		up_direction = Vector3.UP
+		pathPosition -= grindVel * pathDir * delta
 		
+		position = _getPositionOnCurve(path, pathPosition) + up_direction * 0.25
+		curveTangent = _alignToCurve(path, global_position) * -pathDir
+		pathVelocity = curveTangent  * grindVel
+		rotation.y = atan2(curveTangent.x,curveTangent.z)
+		if(input.z):
+			velocity = curveTangent * grindVel
+			velocity += xForm.basis.y * input.z * jumpVel
+			playerState = PlayerState.AIR
+		if (!_getStickCurve(path, global_position)):
+			velocity = curveTangent  * grindVel
+			playerState = PlayerState.AIR
+	else:
+		collision.disabled = false
+	
 	#align upvector with ground while grounded
 	global_transform = _align(global_transform, up_direction)
-
 	#set player parameters for next physics iteration
 	lastPlayerState = playerState
 	lastPhysicsPosition = global_position
-	
 	#apply movement
 	move_and_slide()
-	
-
 
 func _playerState():
 	
 	for body in area.get_overlapping_bodies():
 		if(body.is_in_group("rampRail")):
 			path = body.get_child(0)
+			pathLength = path.curve.get_baked_length()
 			if inputTricks.x == 1:
+				pathDir = _getCurveDir(path, position)
+				pathPosition = _getClosestCurveOffset(path, position)
 				playerState = PlayerState.GRIND
 			#print(body.get_path_node())
 			#todo, get the path by the node and not by the child
@@ -134,11 +146,6 @@ func _playerState():
 
 	#setting player states	
 	if (playerState == PlayerState.FALL):
-		return
-		
-	if(is_on_wall()):
-		playerState = PlayerState.FALL
-		_fall()
 		return
 		
 	if(playerState == PlayerState.GRIND):
@@ -158,19 +165,22 @@ func _playerState():
 		if(collInfo):
 			groundNormal = (collInfo.get_normal() * Vector3(1,0,1)).normalized()
 	
+	if is_on_wall():
+		playerState = PlayerState.FALL
+		_fall()
+	
 	if !is_on_floor():
 		#behavior while in air, or sticked to a pipe
 		if(abs(xForm.basis.z.dot(Vector3.UP)) > 0.25 and playerState == PlayerState.PIPE and input.z == 0):
 			playerState = PlayerState.PIPESNAP
 			rampPos = global_position - get_last_motion() - Vector3.UP * 0.2
 		if(playerState != PlayerState.PIPESNAP):
-			playerState = PlayerState.AIR	
-			
+			playerState = PlayerState.AIR			
 	#set current collision layer als last collision layer for next physics cycle	
 	lastCollLayer = collLayer
-func _alignToCurve(path: Path3D, pos: Vector3):
-	var curve: Curve3D = path.curve
 	
+func _alignToCurve(path: Path3D, pos: Vector3):
+	var curve: Curve3D = path.curve	
 	#sample curve for last physics iteration
 	var pathTransform: Transform3D = path.global_transform
 	var lastLocalPos: Vector3 = lastPhysicsPosition * pathTransform
@@ -180,7 +190,6 @@ func _alignToCurve(path: Path3D, pos: Vector3):
 	var lastCurvePos: Vector3 = curve.sample_baked(lastOffset, true)
   # transform it back to world space
 	lastCurvePos = pathTransform * lastCurvePos
-	
 	#sample curve for current physics iteraiton
 	var localPos: Vector3 = pos * pathTransform
   # get the nearest offset on the curve
@@ -189,11 +198,23 @@ func _alignToCurve(path: Path3D, pos: Vector3):
 	var curvePos: Vector3 = curve.sample_baked(offset, true)
   # transform it back to world space
 	curvePos = pathTransform * curvePos
-
 	var tangent = (curvePos - lastCurvePos).normalized()
 	if(offset - lastOffset < 0):
 		tangent *= Vector3(-1,-1,-1)
 	return tangent
+	
+func _getCurveDir(path: Path3D, pos: Vector3):
+	#function to get direction along the curve, based on the starting direction
+	var curve: Curve3D = path.curve	
+	var currentPoint =  _getClosestCurvePoint(path, pos)
+	var nextPoint = _getClosestCurvePoint(path, pos + velocity)
+	var dir = curve.get_closest_offset(currentPoint) - curve.get_closest_offset(nextPoint)
+	if(dir > 0.1):
+		return  1
+	if(dir < -0.1):
+		return -1
+	else:
+		return 0
 
 func _getClosestCurvePoint(path: Path3D,pos: Vector3):
 	var curve: Curve3D = path.curve
@@ -207,6 +228,29 @@ func _getClosestCurvePoint(path: Path3D,pos: Vector3):
   # transform it back to world space
 	curvePos = pathTransform * curvePos
 	return curvePos
+
+func _getClosestCurveOffset(path: Path3D, pos: Vector3):
+	var curve: Curve3D = path.curve
+	var pathTransform: Transform3D = path.global_transform
+	var localPos: Vector3 = pos * pathTransform
+	var offset: float = curve.get_closest_offset(localPos)
+	return offset
+	
+func _getPositionOnCurve(path: Path3D, offset):
+	var curve: Curve3D = path.curve
+	var curvePos: Vector3 = curve.sample_baked(offset, true)
+	return curvePos
+	
+func _getStickCurve(path: Path3D,pos: Vector3):
+	var curve: Curve3D = path.curve
+	var pathTransform: Transform3D = path.global_transform
+	var localPos: Vector3 = pos * pathTransform
+	var offset: float = curve.get_closest_offset(localPos)
+	#print(offset)
+	if(offset <= 0.1 or offset >= curve.get_baked_length() -.1):
+		return false
+	else:
+		return true
 		
 func _setUpDirection():
 	#raycast to define new up direction based on the ground
--- Skategame/Scripts/debugView.gd
+++ Skategame/Scripts/debugView.gd
@@ -7,16 +7,19 @@ func _process(delta):
 	queue_redraw()
 
 func _draw():
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.x, Color.GREEN)
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED)
-	_debugDraw(player.global_position, player.global_position + player.transform.basis.z, Color.BLUE)
-	_debugDraw(player.global_position, player.global_position + player.velocity * 0.25, Color.PURPLE)
-	_debugDraw(player.global_position, player.global_position + player.up_direction * 2, Color.PINK)
-	_debugDraw(player.curveSnap, player.curveSnap + Vector3.UP * 2, Color.PINK)
-	_debugDraw(player.global_position, player.global_position + player.curveTangent * 2, Color.BLUE)
+	_debugDraw(player.global_position, player.global_position + player.transform.basis.x, Color.GREEN, 2.0)
+	_debugDraw(player.global_position, player.global_position + player.transform.basis.y, Color.RED, 2.0)
+	_debugDraw(player.global_position, player.global_position + player.transform.basis.z, Color.BLUE ,2.0)
+	if (player.playerState != player.PlayerState.GRIND):
+		_debugDraw(player.global_position, player.global_position + player.velocity * 0.25, Color.PURPLE, 4.0)
+	else:
+		_debugDraw(player.global_position, player.global_position + player.pathVelocity * 0.25, Color.PURPLE, 4.0)
+	_debugDraw(player.global_position, player.global_position + player.up_direction * 2, Color.PINK, 2.0)
+	_debugDraw(player.curveSnap, player.curveSnap + Vector3.UP * 2, Color.PINK, 2.0)
+	#_debugDraw(player.global_position, player.global_position + player.curveTangent * 2, Color.BLUE, 2.0)
 	
-func _debugDraw(from, to, col):
-	draw_line(player.camera.unproject_position(from), player.camera.unproject_position(to), col, 2.0)
+func _debugDraw(from, to, col, thickness):
+	draw_line(player.camera.unproject_position(from), player.camera.unproject_position(to), col, thickness)
 
 
 
