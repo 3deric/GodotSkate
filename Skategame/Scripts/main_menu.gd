f4406c54c467260d5b414d893729163727b865c5
Eric <eric.schubert.3d@gmail.com>
2025-02-13 18:09:08 +0100

    Added prototype menu panels for character customization

---
+++ Skategame/Scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+func _ready() -> void:
+	_set_icons()
+	
+	
+func _set_icons():
+	pass
+	#var _container : TabContainer = $HBoxContainer/TabContainer
+	#_container.set_tab_icon(0, load("res://Assets/UI/button1.png"))
+	#_container.set_tab_title(0, "")
+	#_container.set_tab_icon(1, load("res://Assets/UI/button2.png"))
+	#_container.set_tab_title(1, "")
+	#_container.set_tab_icon(2, load("res://Assets/UI/button3.png"))
+	#_container.set_tab_title(2, "")
+	#_container.set_tab_icon(3, load("res://Assets/UI/button3.png"))
+	#_container.set_tab_title(3, "")
+	#_container.set_tab_icon(4, load("res://Assets/UI/buttonA.png"))
+	#_container.set_tab_title(4, "")
+	#_container.set_tab_icon(5, load("res://Assets/UI/buttonB.png"))
+	#_container.set_tab_title(5, "")
+	#_container.set_tab_icon(6, load("res://Assets/UI/button1.png"))
+	#_container.set_tab_title(6, "")
